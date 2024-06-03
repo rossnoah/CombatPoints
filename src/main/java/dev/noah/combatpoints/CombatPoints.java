32c75eb85dbd830c502932ad208f7d18b64b7cb9
Noah Ross <me@noah.dev>
2024-06-03 12:21:04 -0700

    Add command and config

--- src/main/java/dev/noah/combatpoints/CombatPoints.java
+++ src/main/java/dev/noah/combatpoints/CombatPoints.java
@@ -9,6 +9,15 @@ public final class CombatPoints extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
 
+        this.saveDefaultConfig();
+        boolean isValidConfig = verifyConfig();
+
+        if(!isValidConfig){
+            getLogger().warning("Invalid config, disabling plugin.");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+
         if (Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) { //
             new CombatPointsPlaceholder(this).register(); //
         }else{
@@ -16,7 +25,29 @@ public final class CombatPoints extends JavaPlugin {
         }
 
         Bukkit.getPluginManager().registerEvents(new DeathListener(),this);
+        getCommand("combatpoints").setExecutor(new CombatPointsCommand(this));
 
+    }
+
+    private boolean verifyConfig(){
+        boolean valid = true;
+        if(!this.getConfig().contains("messages.no-permission")){
+            getLogger().warning("Missing no-permission message in config.");
+            valid = false;
+        }
+        if(!this.getConfig().contains("messages.no-console")){
+            getLogger().warning("Missing no-console message in config.");
+            valid = false;
+        }
+        if(!this.getConfig().contains("messages.points.self")){
+            getLogger().warning("Missing points.self message in config.");
+            valid = false;
+        }
+        if(!this.getConfig().contains("messages.points.other")){
+            getLogger().warning("Missing points.other message in config.");
+            valid = false;
+        }
+        return valid;
     }
 
     @Override
---
+++ src/main/java/dev/noah/combatpoints/CombatPointsCommand.java
@@ -0,0 +1,53 @@
+package dev.noah.combatpoints;
+
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class CombatPointsCommand implements CommandExecutor {
+
+
+    private final CombatPoints plugin;
+    public CombatPointsCommand(CombatPoints plugin){
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+
+        MiniMessage miniMessage = MiniMessage.miniMessage();
+        if(strings.length == 0){
+            //check if its a player
+            if(commandSender instanceof Player){
+                if(!commandSender.hasPermission("combatpoints.command.self")){
+                    commandSender.sendMessage(miniMessage.deserialize(plugin.getConfig().get("messages.no-permission").toString()));
+                    return true;
+                }
+                Player player = (Player) commandSender;
+                int points = PDCUtils.getPoints(player);
+                player.sendMessage(miniMessage.deserialize(plugin.getConfig().get("messages.points.self").toString().replace("%points%",String.valueOf(points))));
+                return true;
+            }else{
+                commandSender.sendMessage(miniMessage.deserialize(plugin.getConfig().get("messages.no-console").toString()));
+                return true;
+            }
+        }else{
+            if(!(commandSender.hasPermission("combatpoints.command.other"))) {
+                commandSender.sendMessage(MiniMessage.miniMessage().deserialize(plugin.getConfig().get("messages.no-permission").toString()));
+                return true;
+            }
+                Player target = plugin.getServer().getPlayer(strings[0]);
+                if(target == null){
+                    commandSender.sendMessage("Player not found.");
+                    return true;
+                }
+                int points = PDCUtils.getPoints(target);
+                commandSender.sendMessage(miniMessage.deserialize(plugin.getConfig().get("messages.points.other").toString().replace("%player%",target.getName()).replace("%points%",String.valueOf(points))));
+                return true;
+            }
+
+    }
+}
